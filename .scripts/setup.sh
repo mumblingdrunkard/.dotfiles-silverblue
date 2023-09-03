e85d710ceaff317edf280da3527f0222fd826517
mumblingdrunkard <mumblingdrunkard@dev.fedora>
2023-09-03 19:00:22 +0200

    More fixes

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -8,10 +8,12 @@ flatpak install com.github.geigi.cozy                \
                 com.mattjakeman.ExtensionManager     \
                 dev.alextren.Spot                    \
                 me.kozec.syncthingtk                 \
-                org.mozilla.Firefox                  \
-                org.freedesktop.Platform.ffmpeg-full \
                 org.videolan.VLC
 
-[ -d "~/powerlevel10k" ] && echo p10k dir does not exist && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 1 | flatpak install flathub org.freedesktop.Platform.ffmpeg-full
 
-[ -f "~/.ssh/id_ed25519.pub" ] && echo ssh key does not exist && ssh-keygen -t ed25519
+[ ! -d "powerlevel10k" ] && echo p10k dir does not exist && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+[ ! -f ".ssh/id_ed25519.pub" ] && echo ssh key does not exist, creating... && ssh-keygen -t ed25519
+
+[ ! -d "WhiteSur-gtk-theme" ] && echo WhiteSur theme not installed, installing... && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1 && echo y | distrobox create tmp && distrobox enter tmp -e "./WhiteSur-gtk-theme/install.sh && exit"
