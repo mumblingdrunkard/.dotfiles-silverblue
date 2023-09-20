2d6cda7ddd9d1740fed29154df33f8640a8b6e1a
mumblingdrunkard <mumblingdrunkard@dev.dhcp-10-24-2-77.wlan.ntnu.no>
2023-09-20 16:29:36 +0200

    Update some configs

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-rpm-ostree install distrobox zsh gnome-tweaks syncthing
+# rpm-ostree install distrobox zsh gnome-tweaks syncthing
 
-rpm-ostree override remove firefox-langpacks firefox toolbox
+# rpm-ostree override remove firefox-langpacks firefox toolbox
 
-flatpak install com.github.geigi.cozy                \
-                com.mattjakeman.ExtensionManager     \
-                dev.alextren.Spot                    \
-                me.kozec.syncthingtk                 \
-                org.videolan.VLC
+# flatpak install com.github.geigi.cozy                \
+#                 com.mattjakeman.ExtensionManager     \
+#                 dev.alextren.Spot                    \
+#                 me.kozec.syncthingtk                 \
+#                 org.videolan.VLC
 
-echo 1 | flatpak install flathub org.freedesktop.Platform.ffmpeg-full
+# echo 1 | flatpak install flathub org.freedesktop.Platform.ffmpeg-full
 
 [ ! -d "powerlevel10k" ] && echo p10k dir does not exist && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 [ ! -f ".ssh/id_ed25519.pub" ] && echo ssh key does not exist, creating... && ssh-keygen -t ed25519
 
-[ ! -d "WhiteSur-gtk-theme" ] && echo WhiteSur theme not installed, installing... && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1 && echo y | distrobox create tmp && distrobox enter tmp -e "./WhiteSur-gtk-theme/install.sh && exit"
+# [ ! -d "WhiteSur-gtk-theme" ] && echo WhiteSur theme not installed, installing... && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1 && echo y | distrobox create tmp && distrobox enter tmp -e "./WhiteSur-gtk-theme/install.sh && exit"
--- Distrobox/rebuild.sh
+++ Distrobox/rebuild.sh
@@ -1,25 +1,7 @@
 cd $HOME
 
-# Delete old images/containers
-distrobox rm --force dev
-podman rmi --force dev
+. Distrobox/build-container.sh
 
-# Rebuild
-podman build Distrobox --tag dev
-distrobox assemble create
+. Distrobox/update-config.sh
 
-# Create container home in case it does not already exist
-mkdir  .local/share/distrobox/home/dev/.config -p
-
-# Copy configs into container home, overwrites current config
-cp -r  .config/helix .local/share/distrobox/home/dev/.config
-cp     .zshrc        .local/share/distrobox/home/dev
-cp     .p10k.zsh     .local/share/distrobox/home/dev
-rm -rf .local/share/distrobox/home/dev/powerlevel10k 
-cp -r  powerlevel10k .local/share/distrobox/home/dev
-cp     .tmux.conf    .local/share/distrobox/home/dev
-
-# Append direnv hook to .zshrc as direnv is not in host system, but should be in dev container
-echo ''                           >> .local/share/distrobox/home/dev/.zshrc
-echo '# Set up direnv'            >> .local/share/distrobox/home/dev/.zshrc
-echo 'eval "$(direnv hook zsh)"' >> .local/share/distrobox/home/dev/.zshrc
+. Distrobox/misc.sh
